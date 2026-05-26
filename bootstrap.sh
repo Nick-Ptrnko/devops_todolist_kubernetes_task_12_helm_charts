2b6e15edfdabe7600b7b89fdf82637fdf4e0db9c
Mykola Petrenko <mykola.petrenko@gmail.com>
2026-05-26 12:30:59 +0300

    Before templating

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+kind create cluster --config cluster.yml
+
+kubectl taint nodes -l app=mysql app=mysql:NoSchedule
+
 kubectl apply -f .infrastructure/mysql/ns.yml
 kubectl apply -f .infrastructure/mysql/configMap.yml
 kubectl apply -f .infrastructure/mysql/secret.yml
@@ -15,6 +20,21 @@ kubectl apply -f .infrastructure/app/nodeport.yml
 kubectl apply -f .infrastructure/app/hpa.yml
 kubectl apply -f .infrastructure/app/deployment.yml
 
+echo "Waiting for TodoApp pods to be ready..."
+sleep 120
+
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-# kubectl apply -f .infrastructure/ingress/ingress.yml
+
+# Force Ingress Controller to run on the control-plane node
+kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type='json' -p='[
+  {"op": "replace", "path": "/spec/template/spec/nodeSelector", "value": {"ingress-ready": "true"}},
+  {"op": "add", "path": "/spec/template/spec/tolerations/-", "value": {"key": "node-role.kubernetes.io/control-plane", "operator": "Exists", "effect": "NoSchedule"}},
+  {"op": "add", "path": "/spec/template/spec/tolerations/-", "value": {"key": "node-role.kubernetes.io/master", "operator": "Exists", "effect": "NoSchedule"}}
+]'
+
+echo "Waiting for Ingress Controller pods..."
+sleep 120
+
+echo "Applying Ingress rules..."
+kubectl apply -f .infrastructure/ingress/ingress.yml
